3eeb577520d3a3e8e6009bb6484eba99e7a90658
YEOBLEE <mauyeoblee@gmail.com>
2023-09-12 19:17:44 +0900

    Update FunctionalInterfaceMain.java

--- java14.lambda/src/com/lec/ex03_return/FunctionalInterfaceMain.java
+++ java14.lambda/src/com/lec/ex03_return/FunctionalInterfaceMain.java
@@ -18,7 +18,7 @@ public class FunctionalInterfaceMain {
 		
 		// 매개변수가 한 개 이상일 경우 소괄호 생략 불가
 		// return문이 있을 경우 실행문이 하나여도 중괄호 생략 불가
-		// 대신 return문을 생략할 수 있음
+		// 대신 return문을 생략할 수 있음 
 		fi = (a, b) ->  a + b; 
 		System.out.println("fi.add(30, 10) = " + fi.mymethod(30, 10));
 		
